4e81084431a8041030596201eb47989462c11c04
zomawia.sailo <zomawia.sailo@SEA-8E40F1>
2017-03-15 12:07:58 -0700

    gah

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 followOffset;
+
+    private void Start()
+    {
+        followOffset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position = target.position + followOffset;
+    }
+}
---
+++ Assets/Scripts/PlayerJoints.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJoints : MonoBehaviour {
+
+    public GameObject player;
+
+    bool hasJoint;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.gameObject.GetComponent<Rigidbody>() != null && !hasJoint)
+            {
+                gameObject.AddComponent<FixedJoint>();
+                gameObject.GetComponent<FixedJoint>().connectedBody = player.GetComponent<Rigidbody>();
+                hasJoint = true;
+            }
+        }
+    }
+}
