d2730b1290cbbb20c314f5ad422b382269c4791d
zomawia.sailo <zomawia.sailo@SEA-8E40F1>
2017-03-20 17:52:46 -0700

    3terahydf

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,36 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    enum FORMSTATES
+    {
+        BLOB, STAY, LINE
+    }
+
+    FORMSTATES unitStates;
+
     public float thrust;
     public float jumpStrength = 2.0f;
     public Rigidbody rb;
+
+    public float turnSpeed = 50f;
 
     public float shootStrength = 5.0f;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-	}
+        
+        unitStates = FORMSTATES.BLOB;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey("w"))
+
+        if (Input.GetKeyUp("e"))
         {
-            rb.AddTorque(thrust,0,0);
+            unitStates++;
+        }
+        else if (Input.GetKeyUp("q"))
+            {
+                unitStates--;
+            }
+
+        if (Input.GetKey("w"))
+        {
+
+            rb.AddForceAtPosition(
         }
 
         if (Input.GetKey("s"))
         {
-            rb.AddTorque(-thrust, 0, 0);
+            rb.AddForce(Vector3.forward * -thrust, ForceMode.Impulse);
         }
 
         if (Input.GetKey("a"))
         {
-            rb.AddTorque(0, 0, thrust);
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey("d"))
         {
-            rb.AddTorque(0, 0, -thrust);
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey("space"))
@@ -42,14 +64,27 @@ public class PlayerController : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(0)) //shoot ur units
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            Ray playerRay = new Ray(transform.position, ray.origin);                      
+            if (unitStates == FORMSTATES.BLOB)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            gameObject.GetComponent<Selector>().selectorList[0].gameObject.GetComponent<Rigidbody>().AddForce(playerRay.direction * shootStrength, ForceMode.Impulse);
-            gameObject.GetComponent<Selector>().selectorList[0].gameObject.GetComponent<UnitProperties>().isAttached = false;
-            gameObject.GetComponent<Selector>().selectorList.RemoveAt(0);
-            
+                Ray playerRay = new Ray(transform.position, ray.origin);
+
+                gameObject.GetComponent<Selector>().selectorList[0].gameObject.GetComponent<Rigidbody>().AddForce(playerRay.direction * shootStrength, ForceMode.Impulse);
+                gameObject.GetComponent<Selector>().selectorList[0].gameObject.GetComponent<UnitProperties>().isAttached = false;
+                gameObject.GetComponent<Selector>().selectorList.RemoveAt(0);
+            }
+
+            if (unitStates == FORMSTATES.LINE)
+            {
+
+            }
+
+            if (unitStates == FORMSTATES.STAY)
+            {
+
+            }
         }
 
         Debug.DrawLine(transform.forward, transform.forward * 55);
